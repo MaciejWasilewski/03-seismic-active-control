ff6d2270d320a233cbe016df8dd71c11781c3c0d
Maciej Wasilewski <maciej0wasilewski@gmail.com>
2019-01-14 16:32:57 +0100

    changed results

--- current_work/write_to_file.m
+++ current_work/write_to_file.m
@@ -1,17 +1,26 @@
 mkdir('results');
+addpath('../control');
 d=10;
 for i=1:1:length(results)
-    mkdir(['results\',results{i}.fname]);
-    csvwrite(['results\',results{i}.fname,'\disturbance.txt'],results{i}.disturbance(1:d:end,:));
+    sdir=fullfile('results',results{i}.fname);
+    mkdir(sdir);
+    csvwrite(fullfile(sdir,'disturbance.txt'),results{i}.disturbance(1:d:end,:));
     [fs, amps]=fft_amps(0.01,results{i}.disturbance(:,2));
-    csvwrite(['results\',results{i}.fname,'\fft.txt'],[fs(1:1:end,:), amps(1:1:end,:)]);
+    csvwrite(fullfile(sdir,'fft.txt'),[fs(1:1:end,:), amps(1:1:end,:)]);
     for j=1:1:length(results{i}.results)
-        mkdir(['results\',results{i}.fname,'\',results{i}.results{j}.name]);
-        csvwrite(['results\',results{i}.fname,'\',results{i}.results{j}.name,'\state.txt'],[results{i}.results{j}.time(1:d:end,:), results{i}.results{j}.state(1:d:end,:)]);
-        temp=results{i}.results{j}.state*matC';
-        csvwrite(['results\',results{i}.fname,'\',results{i}.results{j}.name,'\accels.txt'],[results{i}.results{j}.time(1:d:end,:), temp(1:d:end,:)]);
-        csvwrite(['results\',results{i}.fname,'\',results{i}.results{j}.name,'\J.txt'],[results{i}.results{j}.time(1:d:end,:), results{i}.results{j}.J(1:d:end)']);
-        csvwrite(['results\',results{i}.fname,'\',results{i}.results{j}.name,'\unorm.txt'],results{i}.results{j}.u_norm/results{i}.results{1}.u_norm);
-        csvwrite(['results\',results{i}.fname,'\',results{i}.results{j}.name,'\Jnorm.txt'],results{i}.results{j}.J(end)/results{i}.results{1}.J(end));
+        algDir=fullfile(sdir,results{i}.results{j}.name);
+        mkdir(algDir);
+        csvwrite(fullfile(algDir,'state.txt'),...
+            [results{i}.results{j}.time(1:d:end,:), ...
+            results{i}.results{j}.state(1:d:end,:)]);
+        temp=diff(...
+            results{i}.results{j}.state(1:end,21:40),1,1)./diff(results{i}.results{j}.time(1:end,:),1,1);
+%         results{i}.results{j}.state*matC';
+        csvwrite(fullfile(algDir,'accels.txt'),...
+            [results{i}.results{j}.time(1:d:end-1,:), temp(1:d:end,:)]);
+        csvwrite(fullfile(algDir,'J.txt'),[results{i}.results{j}.time(1:d:end,:), results{i}.results{j}.J(1:d:end)']);
+        csvwrite(fullfile(algDir,'unorm.txt'),results{i}.results{j}.u_norm/results{i}.results{1}.u_norm);
+        csvwrite(fullfile(algDir,'Jnorm.txt'),results{i}.results{j}.J(end)/results{i}.results{1}.J(end));
+        csvwrite(fullfile(algDir,'criteria.txt'),evalCrit(results{i}.results{j}.state(1:end,:)',results{i}.results{j}.time(1:end,:)',diag(matMass)'));
     end
 end
